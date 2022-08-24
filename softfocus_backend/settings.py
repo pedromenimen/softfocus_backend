5ba359c126e2a0a78e1d70151264f362daa32523
Pedro <pedromenimen@gmail.com>
2022-08-24 16:58:55 -0300

    fix: fixed allowed hosts

--- softfocus_backend/settings.py
+++ softfocus_backend/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://softfocus.herokuapp.com"]
 
 
 # Application definition
